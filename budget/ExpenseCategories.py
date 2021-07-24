d9db5d6e84646c9c8d5ae95664da83503c569e40
Cotldus <78590013+cotldus@users.noreply.github.com>
2021-07-24 22:10:53 +0800

    mod 5

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,5 +1,6 @@
 from . import Expense
 import matplotlib.pyplot as plt
+import timeit
 
 def main():
     expenses = Expense.Expenses()
@@ -16,7 +17,41 @@ def main():
     for a,b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
+    
+    #6.2-4
+    print(timeit.timeit(stmt = "expenses.categorize_for_loop()",
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
+                        number=100000,
+                        globals=globals()))
+    
+    #6.5 Duplicate the timeit.timeit() call for set comprehension
+    print(timeit.timeit(stmt = "expenses.categorize_set_comprehension()",
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
+                        number=100000,
+                        globals=globals()))
 
+    # 6.6- 6.7
+    fig, ax = plt.subplots()
+    labels = ['Necessary', 'Food', 'Unnecessary']
+
+    # 6.8-11 Sum the amounts in each set
+    divided_expenses_sum = []
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append(sum(x.amount for x in category_exps))
+    
+    ax.pie(divided_expenses_sum, labels = labels, autopct = '%1.1f%%') # This will format the percentage
+    
+    plt.show()
 
 if __name__ == "__main__":
     main()
